cee61e7a3a837fcdd11af16eb1fde7bedb8d8fe8
SOSE stargate <scotty9897@hotmail.com.au>
2020-05-03 22:49:25 +1000

    just work

---
+++ repos/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fizz = 3;
+            int Buzz = 5;
+            int FizzBuzz = Fizz * Buzz;
+            int numberList = 100;
+
+            for (int i = 1; i <= numberList; i++)
+            {
+                if (i % FizzBuzz == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % Fizz == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % Buzz == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ repos/Learningagain/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Learningagain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am smol, plz don't hurt me");
+            Console.ReadLine();
+
+
+        }
+    }
+}
